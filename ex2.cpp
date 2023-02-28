660fe450c23414fd16cd5da94d419178d62bef27
Kristian <125973595+KiRkAta-wE@users.noreply.github.com>
2023-02-28 12:35:20 +0200

    Add files via upload

---
+++ ex2.cpp
@@ -0,0 +1,16 @@
+#include <iostream>
+struct Student
+{
+    char name[16];
+    char surname[16];
+    char egn[16];
+};
+struct University
+{
+    char name[32];
+    char *students
+};
+int main()
+{
+    
+}
